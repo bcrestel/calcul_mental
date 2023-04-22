9eb8c9b2272f185da455f25dc9618b0463cb9486
Ben Crestel <ben.crestel@zoho.com>
2023-04-22 00:22:49 -0400

    stuck, abnormal behavior from streamlit

--- src/hello_world.py
+++ src/hello_world.py
@@ -1,8 +1,36 @@
-# hello_world.py.py
-# Benjamin Crestel, 2020-07-01
+# hello_world.py
 
 import streamlit as st
+import numpy as np
+import pandas as pd
+import time
 
-x = st.slider("Select a value")
-st.write(x, "squared is", x * x)
-st.write(x, "cubed is", x * x * x)
+
+@st.cache_data
+def get_numbers(step):
+    np.random.seed(int(time.time()))
+    return np.random.randint(0, 10, 20, dtype=int).reshape((-1, 2))
+
+#if "data" not in st.session_state:
+#    cols = ["a", "b", "reponse", "solution"]
+#    st.session_state.data = pd.DataFrame(columns=cols)
+if "idx" not in st.session_state:
+    st.session_state.idx = 0
+
+numbers = get_numbers(0)
+a, b = numbers[st.session_state.idx]
+st.write(f"idx={st.session_state.idx}, a={a}, b={b}")
+
+st.write(f"{a} x {b} = ")
+reponse = st.text_input("Reponse")
+
+run = st.button('Submit')
+
+if run:
+    st.write(f"idx={st.session_state.idx}, a={a}, b={b}, reponse={reponse}")
+    #st.session_state.data.loc[st.session_state.idx] = [a, b, reponse, a*b]
+    st.session_state.idx += 1
+    a, b = numbers[st.session_state.idx]
+    st.write(f"idx={st.session_state.idx}, a={a}, b={b}")
+
+#st.dataframe(st.session_state.data)
